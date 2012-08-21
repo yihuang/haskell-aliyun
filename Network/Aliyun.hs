4c2c272b8df1fba3a4e103c62865337939503f76
yihuang <yi.codeplayer@gmail.com>
2012-08-22 01:11:21 +0800

    cabal files

--- Network/Aliyun.hs
+++ Network/Aliyun.hs
@@ -5,7 +5,6 @@ import qualified Prelude
 import BasicPrelude
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as S
-import qualified Data.ByteString.Base64 as B64
 
 import Control.Monad.Trans.Resource (ResourceT)
 import Control.Monad.Trans.Reader
@@ -15,12 +14,12 @@ import Control.Monad.Base
 
 import Data.Default (def)
 import Data.Time (getCurrentTime, formatTime)
-import Data.HMAC (hmac_sha1)
 import qualified Data.Conduit as C
 
 import System.Locale (defaultTimeLocale)
-import qualified Network.HTTP.Types as W
 import Network.HTTP.Conduit
+
+import Network.Aliyun.Utils
 
 data YunConf = YunConf
   { yunHost :: ByteString
@@ -46,20 +45,6 @@ runYun conf yun = runReaderT (unYun yun) conf
 formatNow :: IO ByteString
 formatNow = S.pack . formatTime defaultTimeLocale "%a, %d %b %Y %H:%M:%S GMT" <$> getCurrentTime
 
-authorize :: Request m -> Yun (Request m)
-authorize req = do
-    conf <- askConf
-    let date = fromMaybe "" $ lookup "Date" (requestHeaders req)
-        s = S.concat [ method req
-                     , "\n", "", "\n", "", "\n"
-                     , date, "\n", ""
-                     , path req
-                     ]
-        auth = S.concat [ "OSS ", yunId conf, ":"
-                        , B64.encode $ B.pack (hmac_sha1 (B.unpack (yunKey conf)) (B.unpack s))
-                        ]
-    return $ req { requestHeaders = ("Authorization", auth) : requestHeaders req }
-
 getRequest :: ByteString -> Yun (Request m)
 getRequest path = do
     conf <- askConf
@@ -67,11 +52,9 @@ getRequest path = do
     let req = def { host = yunHost conf
                   , method = "GET"
                   , path = path
-                  , requestHeaders = [ ("Date", time)
-                                     , ("Host", yunHost conf)
-                                     ]
+                  , requestHeaders = [("Date", time)]
                   }
-    authorize req
+    return $ authorizeRequest req (yunId conf) (yunKey conf)
 
 listService :: Yun LByteString
 listService = do
---
+++ Network/Aliyun/Utils.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Network.Aliyun.Utils where
+
+import qualified Prelude
+import BasicPrelude
+
+import qualified Data.ByteString as B
+import qualified Data.CaseInsensitive as CI
+import qualified Data.ByteString.Char8 as S
+import qualified Data.ByteString.Base64 as B64
+import Data.HMAC (hmac_sha1)
+
+import qualified Network.HTTP.Types as W
+import Network.HTTP.Conduit
+
+canonicalizeHeaders :: W.RequestHeaders -> ByteString
+canonicalizeHeaders hds =
+    let hds' = [hd | hd <- hds, "x-oss-" `S.isPrefixOf` CI.foldedCase (fst hd)]
+
+        -- merge grouped headers.
+        merge :: [W.Header] -> W.Header
+        merge [] = error "impossible [canonicalizeHeaders]"
+        merge xs@((name, _):_) = (name, S.concat . intersperse "," $ map snd xs)
+
+        hds'' = map merge . group . sortWith fst $ hds'
+    in  S.concat . intersperse "\n" $ [S.concat [CI.foldedCase k, "=", v] | (k,v) <- hds'']
+
+authorizeRequest :: Request m -> ByteString -> ByteString -> Request m
+authorizeRequest req identity key =
+    let hds = requestHeaders req
+        date = fromMaybe "" $ lookup "Date" hds
+        s = S.concat [ method req
+                     , "\n", "", "\n", "", "\n"
+                     , date, "\n", canonicalizeHeaders hds
+                     , path req
+                     ]
+        auth = S.concat [ "OSS ", identity, ":"
+                        , B64.encode $ B.pack (hmac_sha1 (B.unpack key) (B.unpack s))
+                        ]
+    in  req { requestHeaders = ("Authorization", auth) : requestHeaders req }
